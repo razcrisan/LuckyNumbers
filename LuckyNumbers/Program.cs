a82ba0cc46e6178bd3de23d2cd3faa99fdfadca1
razcrisan <rcrisan13@gmail.com>
2017-10-06 11:49:22 -0400

    part 1, populate array with loop

--- LuckyNumbers/Program.cs
+++ LuckyNumbers/Program.cs
@@ -10,6 +10,42 @@ namespace LuckyNumbers
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Raz's Lucky Numbers Go Big Or Go Broke Lottery");
+            Console.WriteLine("Please select the starting number");
+            int startNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please select the ending number");
+            int endNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Select your 6 \"lucky\" numbers (press enter after each #)");
+           
+            int[] luckyNumbers = new int[6];
+            for (int i = 0; i <= luckyNumbers.Length; i++)
+
+            {
+
+                luckyNumbers[i] = int.Parse(Console.ReadLine());
+
+                if (luckyNumbers[i] > endNumber)
+                {
+                    Console.WriteLine("This number is invalid. Please enter a valid number.");
+                    continue;
+                }
+               
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
